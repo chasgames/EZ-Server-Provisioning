9d4056052582111c1123504b2486c8878e5a2539
chasgames <ge@ooiq.com>
2018-01-15 14:45:04 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,7 +8,8 @@ if [ $(id -u) -eq 0 ]; then
 	else
 		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
 		useradd -m -p $pass $username
-		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+		usermod -aG sudo $username
+		[ $? -eq 0 ] && echo "User has been added to system.. oh and it's a sudo!" || echo "Failed to add a user!"
 	fi
 else
 	echo "Only root may add a user to the system"
