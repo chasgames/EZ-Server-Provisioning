b3afaf00f5ecb3e4ac63dc22402c414775b41336
chasgames <ge@ooiq.com>
2018-01-15 15:13:56 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,16 @@
-if [ $(id -u) -eq 0 ]; then
-	read -p "Enter username : " username
-	read -s -p "Enter password : " password
+#!/bin/sh
+
+# First Update all packages -y for no interactive
+apt-get update -y
+apt-get upgrade -y
+apt-get install htop denyhosts iotop fail2ban openssh-server -y
+sed -i '/^PermitRootLogin[ \t]\+\w\+$/{ s//PermitRootLogin no/g; }' /etc/ssh/sshd_config
+
+if [ $(id -u) -eq 0 ]; then #root only
+	read -s -p "Enter new root password: " rootpassword
+	echo "root:$rootpassword" | chpasswd
+	read -p "Enter a new user : " username
+	read -s -p "Enter a password for this new user : " password
 	egrep "^$username" /etc/passwd >/dev/null
 	if [ $? -eq 0 ]; then
 		echo "$username exists!"
@@ -15,3 +25,8 @@ else
 	echo "Only root may add a user to the system"
 	exit 2
 fi
+read -p "Set a hostname for your server: (etc.. z.mk) don't use _ " myhostname
+hostnamectl set-hostname $myhostname
+
+# Need to restart SSH for root password to take affect.
+service ssh restart
