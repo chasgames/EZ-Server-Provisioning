fc44ce66404f3ae19d0fd5870d41f86fd27260cc
chasgames <ge@ooiq.com>
2018-01-18 16:51:27 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
- bash -c "$(wget -O - https://raw.githubusercontent.com/chasgames/EZ-Server-Provisioning/master/provision.sh)"
+# bash -c "$(wget -O - https://raw.githubusercontent.com/chasgames/EZ-Server-Provisioning/master/provision.sh)"
+
+wget https://raw.githubusercontent.com/chasgames/EZ-Server-Provisioning/master/provision.sh -O ./prov.sh
+chmod +x prov.sh
+./prov.sh
