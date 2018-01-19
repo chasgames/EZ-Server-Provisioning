85058bd0b4af9f37847f0aaa40b94e892892ad2a
chasgames <ge@ooiq.com>
2018-01-19 16:27:55 +0000

    test
    test

--- testdialog.sh
+++ testdialog.sh
@@ -3,3 +3,18 @@
       2 exitcode=$?;
       3 exec 3>&-;
       4 echo $result $exitcode;
+
+dialog --title "Delete file" \
+--backtitle "Linux Shell Script Tutorial Example" \
+--yesno "Are you sure you want to permanently delete \"/tmp/foo.txt\"?" 7 60
+
+# Get exit status
+# 0 means user hit [yes] button.
+# 1 means user hit [no] button.
+# 255 means user hit [Esc] key.
+response=$?
+case $response in
+   0) echo "File deleted.";;
+   1) echo "File not deleted.";;
+   255) echo "[ESC] key pressed.";;
+esac
