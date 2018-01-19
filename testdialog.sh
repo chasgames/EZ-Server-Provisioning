38ba8b6517bc2db27e656b0ab323e0057fd2151c
chasgames <ge@ooiq.com>
2018-01-19 16:21:50 +0000

    Update testdialog.sh

--- testdialog.sh
+++ testdialog.sh
@@ -1,5 +1,5 @@
  exec 3>&1;
-      1 result=$(dialog --inputbox test 0 0 2>&1 1>&3);
-      2 exitcode=$?;
-      3 exec 3>&-;
-      4 echo $result $exitcode;
+ result=$(dialog --inputbox test 0 0 2>&1 1>&3);
+ exitcode=$?;
+ exec 3>&-;
+ echo $result $exitcode;
