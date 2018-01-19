cb50deff165b4091e215ab03e0ed7a14a4ed8fb9
chasgames <ge@ooiq.com>
2018-01-19 16:30:29 +0000

    test
    test

--- testdialog.sh
+++ testdialog.sh
@@ -1,10 +1,3 @@
- exec 3>&1;
-<<<<<<< HEAD
-      1 result=$(dialog --inputbox test 0 0 2>&1 1>&3);
-      2 exitcode=$?;
-      3 exec 3>&-;
-      4 echo $result $exitcode;
-
 dialog --title "Delete file" \
 --backtitle "Linux Shell Script Tutorial Example" \
 --yesno "Are you sure you want to permanently delete \"/tmp/foo.txt\"?" 7 60
@@ -19,9 +12,12 @@ case $response in
    1) echo "File not deleted.";;
    255) echo "[ESC] key pressed.";;
 esac
-=======
- result=$(dialog --inputbox test 0 0 2>&1 1>&3);
- exitcode=$?;
- exec 3>&-;
- echo $result $exitcode;
->>>>>>> 38ba8b6517bc2db27e656b0ab323e0057fd2151c
+
+
+
+exec 3>&1;
+result=$(dialog --inputbox test 0 0 2>&1 1>&3);
+exitcode=$?;
+exec 3>&-;
+echo $result $exitcode;
+
