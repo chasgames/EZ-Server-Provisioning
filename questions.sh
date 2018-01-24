c79e8911efd741884d148a7475d6f5203877946f
chasgames <ge@ooiq.com>
2018-01-24 16:25:58 +0000

    fix2

--- questions.sh
+++ questions.sh
@@ -1,5 +1,19 @@
 cancel_catch=0
 exec 3>&1;
+dialog --title "do you want this app" \
+--backtitle "checking what apps you want to use" \
+--yesno "Install Duo 2FA?" 7 60
+
+        if [[ $? -eq 0 ]] ; then
+        echo "executing"
+            duointegration=$(dialog --inputbox "Login to https://duo.com/
+        Click Applications –> Protect an Application
+        Scroll down to Unix Application and click Protect this Application
+        We will need your integration key, secret key and API hostname.
+
+        Copy and paste your integration key here:" 0 0 2>&1 1>&3);
+        fi
+        
 hostname_choice=$(dialog --inputbox "Enter the hostname for this server (e.g dedust2 .... don't use _)" 0 0 2>&1 1>&3);
     if [[ $? -eq 1 ]]; then
         #  cancel button pressed
@@ -59,19 +73,6 @@ case $response in
    255) cancel_catch=1;;
 esac
 
-dialog --title "do you want this app" \
---backtitle "checking what apps you want to use" \
---yesno "Install Duo 2FA?" 7 60
-
-        if [[ $? -eq 0 ]] ; then
-        echo "executing"
-            duointegration=$(dialog --inputbox "Login to https://duo.com/
-        Click Applications –> Protect an Application
-        Scroll down to Unix Application and click Protect this Application
-        We will need your integration key, secret key and API hostname.
-
-        Copy and paste your integration key here:" 0 0 2>&1 1>&3);
-        fi
 
 
 export cancel_catch=$cancel_catch
