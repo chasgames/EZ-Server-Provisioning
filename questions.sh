1b8b98c359bba50075b07546ccc58fa512169458
chasgames <ge@ooiq.com>
2018-01-24 15:06:55 +0000

    test

--- questions.sh
+++ questions.sh
@@ -33,7 +33,15 @@ newusr_choice=$(dialog --inputbox "Create your user (e.g Charlie)" 0 0 2>&1 1>&3
         	cancel_catch=1
     fi
 newusrpw_choice=$(dialog --insecure --passwordbox "Enter the password for $newusr_choice" 0 0 2>&1 1>&3);
-    if [[ $? -eq 1 ]]; then
+    if [[ $? -eq 0 ]]; then
+        echo "you clearly want duo"
+    duointegration=$(dialog --inputbox "Login to https://duo.com/
+Click Applications –> Protect an Application
+Scroll down to Unix Application and click Protect this Application
+We will need your integration key, secret key and API hostname.
+
+Copy and paste your integration key here:" 0 0 2>&1 1>&3);
+    elif [[ $? -eq 1 ]]; then
         #  cancel button pressed
         	cancel_catch=1
     elif [[ $? -eq 5 ]]; then
@@ -68,26 +76,6 @@ case $response in
    1) duo_choice="no";;
    255) cancel_catch=1;;
 esac
-
-echo "about to execute"
-if [ $duo_choice == "yes" ]; then
-echo "executing"
-exec 3>&1;
-dialog --inputbox "Login to https://duo.com/
-Click Applications –> Protect an Application
-Scroll down to Unix Application and click Protect this Application
-We will need your integration key, secret key and API hostname.
-
-Copy and paste your integration key here:" 0 0 2>&1 1>&3
-    duointegration=$(dialog --inputbox "Login to https://duo.com/
-Click Applications –> Protect an Application
-Scroll down to Unix Application and click Protect this Application
-We will need your integration key, secret key and API hostname.
-
-Copy and paste your integration key here:" 0 0 2>&1 1>&3);
-exitcode=$?;
-exec 3>&-;
-fi
 
 
 
