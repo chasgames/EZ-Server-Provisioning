787a7764cc7edc83f532a99735a591eaec9c500c
chasgames <ge@ooiq.com>
2018-01-24 11:29:52 +0000

    testtt

--- questions.sh
+++ questions.sh
@@ -9,6 +9,13 @@ hostname_choice=$(dialog --inputbox "Enter the hostname for this server (e.g ded
         	cancel_catch=1
     fi
 rootpw_choice=$(dialog --insecure --passwordbox "Change the root password" 0 0 2>&1 1>&3);
+    if [[ $? -eq 1 ]]; then
+        #  cancel button pressed
+        	cancel_catch=1
+    elif [[ $? -eq 5 ]]; then
+        #  timeout
+        	cancel_catch=1
+    fi
 rootpwconfirm_choice=$(dialog --insecure --passwordbox "Enter the password again" 0 0 2>&1 1>&3);
 newusr_choice=$(dialog --inputbox "Create your user (e.g Charlie)" 0 0 2>&1 1>&3);
 newusrpw_choice=$(dialog --insecure --passwordbox "Enter the password for $newusr_choice" 0 0 2>&1 1>&3);
