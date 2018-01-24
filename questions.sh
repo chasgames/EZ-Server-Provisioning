210bce86eca922e3cd91d3c7461a1aef0dda961e
chasgames <ge@ooiq.com>
2018-01-24 15:41:24 +0000

    tet

--- questions.sh
+++ questions.sh
@@ -44,20 +44,6 @@ exitcode=$?;
 exec 3>&-;
 #echo $result $exitcode;
 
-dialog --title "do you want this app" \
---backtitle "checking what apps you want to use" \
---yesno "Install Docker?" 7 60
-
-# Get exit status
-# 0 means user hit [yes] button.
-# 1 means user hit [no] button.
-# 255 means user hit [Esc] key.
-response=$?
-case $response in
-   0) docker_choice="yes";;
-   1) docker_choice="no";;
-   255) cancel_catch=1;;
-esac
 
 dialog --title "do you want this app" \
 --backtitle "checking what apps you want to use" \
@@ -72,6 +58,21 @@ dialog --title "do you want this app" \
 
         Copy and paste your integration key here:" 0 0 2>&1 1>&3);
         fi
+
+dialog --title "do you want this app" \
+--backtitle "checking what apps you want to use" \
+--yesno "Install Docker?" 7 60
+
+# Get exit status
+# 0 means user hit [yes] button.
+# 1 means user hit [no] button.
+# 255 means user hit [Esc] key.
+response=$?
+case $response in
+   0) docker_choice="yes";;
+   1) docker_choice="no";;
+   255) cancel_catch=1;;
+esac
 
 
 
